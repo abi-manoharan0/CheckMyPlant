c4b3a741caaf3dbcc1d863e84fa0f3bede61975f
Kareem Zahra <kts2001@live.com>
2023-02-17 16:43:09 -0500

    Delete image_convertor.py

--- image_convertor.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from keras.utils import img_to_array
-#opencv
-
-def image_convertor(dir):
-    image=cv2.imread(dir)
-    if image is not None:
-        image=cv2.resize(image,(256, 256))
-        return img_to_array(image)

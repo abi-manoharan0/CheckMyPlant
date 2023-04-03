babbb1dae7f17d8d707741742d5f3f2c147a0be1
gordonstevens <gsteven5@my.centennialcollege.ca>
2023-04-03 14:40:31 -0400

    - Simplified directory structure to /static/ and /templates/ - Changed css and moved inline CSS to styles.css - Edited app.py to support flask-style config params - Added host/port/debug=False, use sudo to unblock port on linux - Removed "more" link - Updated bootstrap to use CDN instead of CDN and then localhost; use bootstrap's .bundle.js for contact/feedback pages - Used flask's url_for to reference flask functions in app.py as not to reveal the directory as per flask standard - Tested contact page, works - Created Feedback page - Used flask's url_for for image loading as standard - Cleaned up HTML and CSS a bit

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
+# Import and variables
 import os
-
+import numpy as np
 import tensorflow as tf
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, url_for, send_from_directory
 from PIL import Image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.preprocessing import image
-import numpy as np
-
-
-
 
 app = Flask(__name__)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-UPLOAD_FOLDER = "uploads"
-STATIC_FOLDER = "static"
+app.config['UPLOAD_FOLDER'] = "uploads"
+app.config['STATIC_FOLDER'] = "static"
+sizeofimage = 256
+app_debug = False
+app_host = '0.0.0.0'
+app_port = 80
 
 # Load model
-cauliflower_model = tf.keras.models.load_model(STATIC_FOLDER + "/models/" + "Cauliflower_best_model.h5")
-
-sizeofimage = 256
+cauliflower_model = tf.keras.models.load_model(app.config['STATIC_FOLDER'] + "/model/Cauliflower_best_model.h5")
 
 # Preprocess an image
 def preprocess(image):
@@ -32,7 +31,6 @@ def preprocess(image):
 def load(path):
     image = tf.io.read_file(path)
     return preprocess(image)
-
 
 # Add a pre-screenr to catch image that doesn't belong to the class
 # Load pre-trained ResNet50 model
@@ -68,27 +66,37 @@ def classify(model, image_path):
 
     return label, classified_probability
 
-
-
-
-
-# index page
+# Index page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# About page
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
+# Contact page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# Feedback page
+@app.route("/feedback")
+def feedback():
+    return render_template("feedback.html")
+
+# Classify page
 @app.route("/classify", methods=["POST", "GET"])
 def upload_file():
 
     if request.method == "POST":
         file = request.files["image"]
-        upload_image_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        upload_image_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(upload_image_path)
         label = predict_image(upload_image_path)
         label, probability = classify(cauliflower_model, upload_image_path)
         probability = round((probability * 100), 2)
-
     else:
         if request.method == "GET":
             return render_template("index.html")
@@ -97,12 +105,9 @@ def upload_file():
         "classify.html", imagefile=file.filename, label=label, prob=probability
     )
 
-
-
 @app.route("/classify/<filename>")
 def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
-
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=app_host, port=app_port, debug=app_debug)
